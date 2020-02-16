10d8c87e6cfbb4cd83af1dd53ca7bca702c7c35c
unknown <wanzerox@gmail.com>
2020-02-17 00:06:33 +0700

    aggregation pipeline

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -108,11 +108,95 @@ const deleteTour = async (req, res) => {
   }
 };
 
+const getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } }
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' }
+        }
+      },
+      {
+        $sort: { avgPrice: 1 }
+      }
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: { stats }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+const getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates'
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`)
+          }
+        }
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numTourStart: { $sum: 1 },
+          tours: { $push: '$name' }
+        }
+      },
+      // {
+      //   $addField: { month: '$_id' }
+      // }
+      // $addField: $addField is not allowed in this atlas tier
+      {
+        $project: {
+          _id: 0
+        }
+      },
+      {
+        $sort: { numTourStart: -1 }
+      }
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { plan }
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
 module.exports = {
   getAllTours: getAllTours,
   createNewTour: createNewTour,
   updateTour: updateTour,
   getTour: getTour,
   deleteTour: deleteTour,
-  alias
+  alias,
+  getTourStats,
+  getMonthlyPlan
 };
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -9,6 +9,9 @@ router
   .route('/top-5-tours')
   .get(tourController.alias, tourController.getAllTours);
 
+router.route('/tour-stats').get(tourController.getTourStats);
+router.route('/monthly-plan/:year').get(tourController.getMonthlyPlan);
+
 router
   .route('/')
   .get(tourController.getAllTours)
