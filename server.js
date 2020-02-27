ced3980712f5a8f827394db8d458cd13fce3d2c2
unknown <wanzerox@gmail.com>
2020-02-27 10:48:51 +0700

    commit

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ mongoose
     console.log('DB connection successful!');
   });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}....`);
